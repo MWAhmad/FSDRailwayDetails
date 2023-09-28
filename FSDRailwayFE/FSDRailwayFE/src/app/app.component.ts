a11d16ee48df001101a3ec4b60b69e249da97837
Waleed Ahmad <waleedahmad767747@gmail.com>
2023-09-28 14:55:30 +0500

    Updated files and added header component

--- FSDRailwayFE/FSDRailwayFE/src/app/app.component.ts
+++ FSDRailwayFE/FSDRailwayFE/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'FSDR-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

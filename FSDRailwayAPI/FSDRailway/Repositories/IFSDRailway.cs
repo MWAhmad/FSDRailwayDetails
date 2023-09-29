1b35075d11545b2ac70d6e7d81c2c94150df089f
umar0787 <umariiqbal0787@gmail.com>
2023-09-30 01:42:44 +0500

    API has been completed with Get Methods included

--- FSDRailwayAPI/FSDRailway/Repositories/IFSDRailway.cs
+++ FSDRailwayAPI/FSDRailway/Repositories/IFSDRailway.cs
@@ -1,7 +1,10 @@
-﻿namespace FSDRailway.Repositories
+﻿using FSDRailway.Models;
+
+namespace FSDRailway.Repositories
 {
     public interface IFSDRailway
     {
-
+        Task<List<Train>> GetAllAsync();
+        Task<Train> GetByIdAsync(Guid id);
     }
 }
--- FSDRailwayAPI/FSDRailway/Repositories/SQLFSDRailway.cs
+++ FSDRailwayAPI/FSDRailway/Repositories/SQLFSDRailway.cs
@@ -1,6 +1,28 @@
-﻿namespace FSDRailway.Repositories
+﻿using AutoMapper;
+using FSDRailway.Data;
+using FSDRailway.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSDRailway.Repositories
 {
     public class SQLFSDRailway : IFSDRailway
     {
+        private readonly FSDRailwayDbContext dbContext;
+
+        public SQLFSDRailway(FSDRailwayDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<Train>> GetAllAsync()
+        {
+             return await dbContext.Trains.ToListAsync();
+        }
+
+        public async Task<Train> GetByIdAsync(Guid id)
+        {
+            var train = await dbContext.Trains.FirstOrDefaultAsync(x => x.Id == id);
+            return train;
+        }
     }
 }
